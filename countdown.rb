b7bb74aa894c10df38aa1f5b91fcb473ef7db13d
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-21 23:12:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,8 @@ integer = 10
   while integer > 0
     puts "#{integer} SECOND(S)!"
     integer -= 1
-
- #integer == 0
-  #return "HAPPY NEW YEAR!"
+  else integer = 0
+    puts  "HAPPY NEW YEAR!"
 
 end
 end
